e39a7de4d85e93eb85cae995efe63e6642cd418d
bartech <bartekspitza@hotmail.com>
2024-06-27 19:35:20 +0200

    push markers to source

---
+++ 06 Push Markers To Source.jsx
@@ -0,0 +1,37 @@
+{
+    function transferMarkers() {
+        var activeItem = app.project.activeItem;
+
+        if (activeItem == null || !(activeItem instanceof CompItem)) {
+            alert("Please select a composition.");
+            return;
+        }
+
+        var selectedLayers = activeItem.selectedLayers;
+
+        if (selectedLayers.length != 1 || !(selectedLayers[0] instanceof AVLayer) || !(selectedLayers[0].source instanceof CompItem)) {
+            alert("Please select a single precomp layer.");
+            return;
+        }
+
+        var precompLayer = selectedLayers[0];
+        var precomp = precompLayer.source;
+
+        app.beginUndoGroup("Transfer Markers");
+
+        // Transfer markers from the precomp layer to the precomp
+        var layerMarkers = precompLayer.property("Marker");
+
+        for (var i = 1; i <= layerMarkers.numKeys; i++) {
+            var markerValue = layerMarkers.keyValue(i);
+            var markerTime = layerMarkers.keyTime(i);
+
+            // Create a new marker in the precomp at the corresponding time
+            precomp.markerProperty.setValueAtTime(markerTime - precompLayer.inPoint, markerValue);
+        }
+
+        app.endUndoGroup();
+    }
+
+    transferMarkers();
+}
